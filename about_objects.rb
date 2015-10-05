ffe423e471ef6f9bb86e29c64b2298bf7efb72d4
Alvaro Garcia <derecerca@gmail.com>
2015-10-05 22:56:09 +0200

    green: contemplate the object_id function for small integers

--- about_objects.rb
+++ about_objects.rb
@@ -44,6 +44,8 @@ class AboutObjects < Neo::Koan
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+
+    # Answer: it seems to be f(n) = 2*n + 1
   end
 
   def test_clone_creates_a_different_object
