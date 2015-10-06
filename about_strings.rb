2e4925ed75ba0fceb14b82e7b447f0792ea62b6b
Alvaro Garcia <derecerca@gmail.com>
2015-10-06 06:26:15 +0200

    answer question about shovel (<<) and addition (+=)

--- about_strings.rb
+++ about_strings.rb
@@ -102,6 +102,8 @@ EOS
     #
     # Ruby programmers tend to favor the shovel operator (<<) over the
     # plus equals operator (+=) when building up strings.  Why?
+
+    # Answer: I guess they prefer the immutability rather than speed
   end
 
   def test_double_quoted_string_interpret_escape_characters
